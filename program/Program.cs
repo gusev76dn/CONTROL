76b7ff853a0e33e437e63fd6ea26a154f533f1fe
gusev76 <gusev76dn@mail.ru>
2022-11-29 23:33:17 +0300

    добавили program

---
+++ program/Program.cs
@@ -0,0 +1,45 @@
+﻿string[] FillArray1()
+{
+    Console.WriteLine("Введите элементы массива через пробел");
+    string[] array = Console.ReadLine().Split(" ").ToArray();
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "; ");
+    }
+    Console.WriteLine();
+}
+
+int LengthArray2(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <=3) count++;
+    }
+    return count;
+}
+
+string[] FillArray2(int count, string[] array)
+{
+    string[] array2 = new string[count];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            array2[index] = array[i];
+            index++;
+        }
+    }
+    return array2;
+}
+
+string[] array1 = FillArray1();
+string[] array2 = FillArray2(LengthArray2(array1), array1);
+PrintArray(array1);
+PrintArray(array2);
